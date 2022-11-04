a2ff0baeccec1bf440e174fa39fc0b2f38854fa4
AlexanderRovner <rovner48@gmail.com>
2022-11-04 19:38:51 +0200

    second for() to exceptionCheck

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println(calc(input));
     }
 
-    public static String calc(String input){
+    public static String calc(String input) {
         exceptionCheck(input);
         if (checkIfNumberIsArabic(input)) {
             String[] arrInput = input.split(" ");
@@ -24,7 +24,7 @@ public class Main {
             RomanToArabic roman1 = RomanToArabic.valueOf(arrInput[0]);
             RomanToArabic roman2 = RomanToArabic.valueOf(arrInput[2]);
             int arabicToRoman = calculationReturnResult(roman1.getArabic(), roman2.getArabic(), arrInput[1]);
-            if (arabicToRoman <= 0){
+            if (arabicToRoman <= 0) {
                 try {
                     throw new IOException();
                 } catch (IOException e) {
@@ -40,21 +40,21 @@ public class Main {
     public static String arabicToRomanTranslation(int arabicToRoman) {
         String roman = "";
 
-        TreeMap<Integer,String> map = new TreeMap<>();
-        map.put(1,"I");
-        map.put(4,"IV");
-        map.put(5,"V");
-        map.put(9,"IX" );
-        map.put(10,"X");
-        map.put(40,"XL");
-        map.put(50,"L");
-        map.put(90,"XC");
-        map.put(100,"C");
+        TreeMap<Integer, String> map = new TreeMap<>();
+        map.put(1, "I");
+        map.put(4, "IV");
+        map.put(5, "V");
+        map.put(9, "IX");
+        map.put(10, "X");
+        map.put(40, "XL");
+        map.put(50, "L");
+        map.put(90, "XC");
+        map.put(100, "C");
 //comment for contribution
         do {
             roman += (map.get(map.floorKey(arabicToRoman)));
             arabicToRoman -= map.floorKey(arabicToRoman);
-        }while (arabicToRoman != 0);
+        } while (arabicToRoman != 0);
 
 
         return roman;
@@ -73,13 +73,14 @@ public class Main {
     }
 
     enum RomanToArabic {
-        I(1),II(2),III(3),IV(4),V(5),VI(6),VII(7),VIII(8),IX(9),X(10);
+        I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10);
         final int arabic;
-        RomanToArabic(int arabic){
+
+        RomanToArabic(int arabic) {
             this.arabic = arabic;
         }
 
-        public int getArabic(){
+        public int getArabic() {
             return this.arabic;
         }
 
@@ -110,7 +111,20 @@ public class Main {
             if ((arrInput[i].equals("+")) || (arrInput[i].equals("-")) || (arrInput[i].equals("/"))
                     || (arrInput[i].equals("*"))) {
                 operandFlag++;
-            } else if (checkIfNumberIsArabic(str)) {
+            }
+        }
+        if ((operandFlag != 1) || (arrInput.length != 3)) {
+            try {
+                throw new IOException();
+            } catch (IOException e) {
+                //
+                System.out.println(
+                        "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор");
+                System.exit(0);
+            }
+        }
+        for (int i = 0; i < arrInput.length; i++) {
+            if (checkIfNumberIsArabic(str)) {
                 integerCheck = Integer.parseInt(arrInput[i]);
                 if (integerCheck > 10 || integerCheck < 1) {
                     try {
@@ -123,17 +137,12 @@ public class Main {
                 }
             }
         }
-            if ((operandFlag != 1) || (arrInput.length != 3)) {
-                try {
-                    throw new IOException();
-                } catch (IOException e) {
-                    //
-                    System.out.println(
-                            "throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор");
-                    System.exit(0);
-                }
-            }
-        }
+    }
+
+
+
+
+
 
 
     public static boolean checkIfNumberIsRoman(String str) {
